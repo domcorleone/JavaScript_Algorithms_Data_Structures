66904d29c3d95d62366f81647b668d85f7efbab2
Eder L. Machado <eder.lourenco.machado@gmail.com>
2023-01-24 23:39:47 +0100

    Lesson#01-Creating Comment in JS

---
+++ 01-Comment.js
@@ -0,0 +1,7 @@
+// Create Comment using Javascript
+/*
+    Lesson# 01 - Creating comments in Javascript
+    Author: @DomCorleone
+    Description: Creating comments in Javascript
+    Create Date: January 24, 2023
+*/
