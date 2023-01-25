44af4197cbbfa29a617c66a0c345437f59ae4198
Eder L. Machado <eder.lourenco.machado@gmail.com>
2023-01-25 03:39:09 +0100

    Lessons from 01 to 10 in js

---
+++ 02-DeclareJsVariables.js
@@ -0,0 +1,7 @@
+// Lesson#02 - Declare JavaScript Variables
+/* Data Types in Js:
+   1-undefined; 2-object; 3-bigint; 4-string; 5-boolean; 6-symbol; 7-number; 8-null.
+
+   Variables can contain letters, numbers, symbol $ , _ ; but can't start with numbers and can't even contain spaces
+*/
+var myName;
