daa36ea3de66a29848bec993e20fb6c6f3062adc
Eder L. Machado <eder.lourenco.machado@gmail.com>
2023-02-25 00:50:34 +0100

    Lessons from 41 to 44 in js

---
+++ 43-ManipulateArrayWithPushMethod.js
@@ -0,0 +1,8 @@
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+/*
+push(param1) add an element at the end of the array
+*/
+myArray.push(["dog", 3]);
---
+++ 44-ManipulateArrayWithPopMethod.js
@@ -0,0 +1,6 @@
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+/* pop() remove the last element from an array and return the removed value */
+let removedFromMyArray = myArray.pop();
