16fad1350e7a3ef3e620e7f0248f6f947b75f70e
Eder L. Machado <eder.lourenco.machado@gmail.com>
2023-06-02 01:39:29 +0100

    Change Practices 83 87 101 110 112
    These Practices were not saved correctly...

--- 87-Add_New_Property_To_JS_Object.js
+++ 87-Add_New_Property_To_JS_Object.js
@@ -8,3 +8,5 @@ const myDog = {
   // Adding new property to the object MyDog
   myDog.bark = "woof";
   // myDog["bark"] = "woof";
+  // you can list again the properties of myDog object
+  console.log(myDog);
